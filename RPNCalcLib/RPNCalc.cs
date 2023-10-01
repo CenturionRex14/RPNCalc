96b4d278d3bc3a2d80ec89fced5101b80a844fd9
Nathan King <nathandking97@gmail.com>
2023-10-01 17:53:43 -0600

    Implimented mulitple calculator operator functions. Remaining are Modulo(), SaveMem(), EraseMem(), and Mem().

--- RPNCalcLib/RPNCalc.cs
+++ RPNCalcLib/RPNCalc.cs
@@ -1,49 +1,148 @@
-﻿namespace RPNCalcLib
+﻿/*
+Supported operations:
++ - Adds X and Y
+- - Subtracts X from Y
+/ - Divides Y by X
+* - Multiplies Y by X
+% - Gives remainder of Y / X
+D - Drops X
+S - Swaps X and Y
+<Enter> - View current registers if no proceeding char
+C - Clears registers
+P - Push registers up
+
+Stretch Goals:
+M - Saves X into memory
+V - Prints value in memory
+E - Erases memory (set to 0)
+L - takes place of memory in operation, e.g.:
+    >5M (stores 5 into memory)
+    >6<enter>
+    >L*
+    30
+    >
+*/
+namespace RPNCalcLib
 {
     public record class RPNCalc()
     {
         protected List<double> stack = new();
         protected double mem = 0;
 
-        public double x => (stack.Count > 0) ? stack[^1] : 0;
-        public double y => (stack.Count > 1) ? stack[^2] : 0;
+        public double X => (stack.Count > 0) ? stack[^1] : 0;
+        public double Y => (stack.Count > 1) ? stack[^2] : 0;
 
-        public RPNCalc push(double num)
+        public RPNCalc Push(double num)
         {
             stack.Add(num);
             return this;
         }
 
-        public double drop() {
-            double rtn = x;
-            stack.RemoveAt(stack.Count - 1);
+        public double Drop()
+        {
+            double rtn = 0;
+            if (stack.Count > 0)
+            {
+                rtn = stack[^1];
+                stack.RemoveAt(stack.Count - 1);
+            }
             return rtn;
         }
 
-        public RPNCalc add()
+        public RPNCalc Swap()
         {
-            double temp = drop();
-            stack[^1] += temp;
+            double a = 0;
+            double b = 0;
+            if (stack.Count > 1)
+            {
+                a = stack[^1];
+                b = stack[^2];
+                stack[^1] = b;
+                stack[^2] = a;
+            }
+            else if (stack.Count == 1)
+            {
+                stack.Add(0);
+            }
             return this;
         }
 
-        public RPNCalc subtract() {
-            double temp = drop();
-            stack[^1] -= temp;
+        public RPNCalc Clear()
+        {
+            stack.Clear();
             return this;
         }
 
-        public RPNCalc multiply()
+
+        public RPNCalc Add()
+        {
+            double temp = Drop();
+            if (stack.Count > 0)
+            {
+                stack[^1] += temp;
+            }
+            else
+            {
+                stack.Add(temp);
+            }
+            return this;
+        }
+
+        public RPNCalc Subtract()
+        {
+            double temp = Drop();
+            if (stack.Count > 0)
+            {
+                stack[^1] = stack[^1] - temp;
+            }
+            else
+            {
+                stack.Add(-1*temp);
+            }
+            return this;
+        }
+
+        public RPNCalc Multiply()
+        {
+            double temp = Drop();
+            if (stack.Count > 0)
+            {
+                stack[^1] *= temp;
+            }
+            return this;
+        }
+
+        public RPNCalc Divide()
+        {
+            double temp = Drop();
+            if (stack.Count > 0 && temp != 0)
+            {
+                stack[^1] = stack[^1] / temp;
+            } else if (temp == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return this;
+        }
+
+        public RPNCalc Modulo()
         {
             throw new NotImplementedException();
-            return this;
         }
 
-        public RPNCalc divide()
+        public void SaveMem(double n)
         {
             throw new NotImplementedException();
         }
 
+        public void EraseMem()
+        {
+            throw new NotImplementedException();
+        }
 
+        public double Mem()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- RPNCalcTest/UnitTest1.cs
+++ RPNCalcTest/UnitTest1.cs
@@ -5,38 +5,283 @@ namespace RPNCalcTest
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void TestAdd()
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestPush(double a, double b)
         {
             var calc = new RPNCalc();
-            calc.push(5).push(7).add();
-            Assert.AreEqual(12, calc.x);
-            Assert.AreEqual(0, calc.y);
+
+            calc.Push(a);
+            Assert.AreEqual(a, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(b);
+            Assert.AreEqual(b, calc.X);
+            Assert.AreEqual(a, calc.Y);
         }
 
         [TestMethod]
         [DataRow(0, 0)]
         [DataRow(1, 2)]
         [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
         [DataRow(double.MaxValue, double.MinValue)]
-        public void TestPush(double a, double b) {
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestDrop(double a, double b)
+        {
             var calc = new RPNCalc();
 
-            calc.push(a);
-            Assert.AreEqual(a, calc.x);
-            Assert.AreEqual(0, calc.y);
+            calc.Push(a).Push(b).Drop();
+            Assert.AreEqual(a, calc.X);
 
-            calc.push(b);
-            Assert.AreEqual(b, calc.x);
-            Assert.AreEqual(a, calc.y);
+            calc.Drop();
+            Assert.AreEqual(0, calc.X);
+
+            calc.Drop();
+            Assert.AreEqual(0, calc.X);
+
         }
 
         [TestMethod]
-        public void TestSubtract() {
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestSwap(double a, double b)
+        {
             var calc = new RPNCalc();
-            calc.push(5).push(7).subtract();
-            Assert.AreEqual(-2, calc.x);
-            Assert.AreEqual(0, calc.y);
+
+            calc.Push(a).Push(b).Swap();
+            Assert.AreEqual(a, calc.X);
+            Assert.AreEqual(b, calc.Y);
+
+            calc.Swap();
+            Assert.AreEqual(b, calc.X);
+            Assert.AreEqual(a, calc.Y);
+
+            calc.Drop();
+            Assert.AreEqual(a, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Swap();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(a, calc.Y);
+
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestClear(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            calc.Push(a).Clear();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(a).Push(b).Clear();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Clear().Clear().Clear();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(a).Push(b).Push(a).Push(b).Push(a).Push(b).Clear();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestAdd(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            calc.Push(a).Push(b).Add();
+            Assert.AreEqual(a + b, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Add();
+            Assert.AreEqual(a + b, calc.X);
+            Assert.AreEqual(0, calc.Y);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestSubtract(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            calc.Push(a).Push(b).Subtract();
+            Assert.AreEqual(a - b, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Subtract();
+            Assert.AreEqual(-1 * (a - b), calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Subtract();
+            Assert.AreEqual((a - b), calc.X);
+            Assert.AreEqual(0, calc.Y);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 2)]
+        [DataRow(3, 4)]
+        [DataRow(-1, -2)]
+        [DataRow(-3, -4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestMultiply(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            calc.Push(a).Push(b).Multiply();
+            Assert.AreEqual(a * b, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(a).Multiply();
+            Assert.AreEqual(a * (a * b), calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(b).Multiply();
+            Assert.AreEqual((a * b) * (a * b), calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Multiply();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 0)]
+        [DataRow(0, 1)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestDivide(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            Assert.ThrowsException<DivideByZeroException>(() => calc.Divide());
+
+            calc.Push(a).Push(b);
+            if (b == 0)
+            {
+                Assert.ThrowsException<DivideByZeroException>(() => calc.Divide());
+            }
+            else
+            {
+                calc.Divide();
+                Assert.AreEqual(a / b, calc.X);
+                Assert.AreEqual(0, calc.Y);
+            }
+
+            calc.Push(b);
+            if (b == 0)
+            {
+                Assert.ThrowsException<DivideByZeroException>(() => calc.Divide());
+            }
+            else
+            {
+                calc.Divide();
+                Assert.AreEqual((a / b) / b, calc.X);
+                Assert.AreEqual(0, calc.Y);
+            }
+
+            calc.Push(a);
+            if (a == 0)
+            {
+                Assert.ThrowsException<DivideByZeroException>(() => calc.Divide());
+            }
+            else if (b != 0)
+            {
+                calc.Divide();
+                Assert.AreEqual(((a / b) / b) / a, calc.X);
+                Assert.AreEqual(0, calc.Y);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(0, 2)]
+        [DataRow(3, 0)]
+        [DataRow(1, -2)]
+        [DataRow(-3, 4)]
+        [DataRow(double.MaxValue, double.MinValue)]
+        [DataRow(double.MinValue, double.MaxValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        [DataRow(double.MinValue, double.MinValue)]
+        public void TestMod(double a, double b)
+        {
+            var calc = new RPNCalc();
+
+            if (a == 0 || b == 0)
+            {
+                calc.Push(a).Push(b).Modulo();
+                Assert.AreEqual(0, calc.X);
+                Assert.AreEqual(0, calc.Y);
+                return;
+            }
+
+            calc.Push(a).Push(b).Modulo();
+            Assert.AreEqual(a % b, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Push(a).Modulo();
+            Assert.AreEqual((a % b) % a, calc.X);
+            Assert.AreEqual(0, calc.Y);
+
+            calc.Modulo();
+            Assert.AreEqual(0, calc.X);
+            Assert.AreEqual(0, calc.Y);
         }
     }
 }
